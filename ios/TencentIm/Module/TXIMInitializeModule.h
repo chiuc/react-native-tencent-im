c94c904f5fbd1e4b67194df34169a5cfcd4a32e4
Choi Wing Chiu <docm1234.wc@gmail.com>
2020-08-22 20:48:03 +0800

    remove

--- ios/TencentIm/Module/TXIMInitializeModule.h
+++
@@ -1,9 +0,0 @@
-#import "RCTEventEmitter+TXIMBaseModule.h"
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface TXIMInitializeModule : RCTEventEmitter
-
-@end
-
-NS_ASSUME_NONNULL_END
--- ios/TencentIm/TencentIm.h
+++
@@ -1,5 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface TencentIm : NSObject <RCTBridgeModule>
-
-@end
